1edfcb87f127cad68501e3d3ae4f90711b9972f2
Jon Torres <dgxstore.ve@gmail.com>
2025-08-25 20:01:42 -0400

    Cambios

--- html/hero.php
+++ html/hero.php
@@ -4,8 +4,8 @@
       <div class="container">
         <div class="row gy-4 justify-content-center justify-content-lg-between">
           <div class="col-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center">
-            <h1 data-aos="fade-up">Enjoy Your Healthy<br>Delicious Food</h1>
-            <p data-aos="fade-up" data-aos-delay="100">We are team of talented designers making websites with Bootstrap</p>
+            <h1 data-aos="fade-up"><?= HERO_TEXT_1; ?><br><?= HERO_TEXT_2; ?></h1>
+            <p data-aos="fade-up" data-aos-delay="100"><?= HERO_TEXT_3; ?></p>
             <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
               <a href="#book-a-table" class="btn-get-started "><?php echo BUTTON_TEXT; ?></a>
               <a href="<?= URL_YOUTUBE_VIDEO?>" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span><?= TXT_WATCH_VIDEO?></span></a>
